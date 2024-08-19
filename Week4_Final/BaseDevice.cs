3b817e56d2c9d482644bd9bcd63d9162bace6e33
Zeynep Tuana Çakır <2320711004@ogrenci.ibu.edu.tr>
2024-08-19 11:19:13 +0300

    Add project files.

---
+++ Week4_Final/BaseDevice.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Final
+{
+    public abstract class BaseDevice
+    {
+        public string SerialNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string OperatingSystem { get; set; }
+
+        public DateTime ProductionDate { get; set; }
+       
+        public BaseDevice() {
+            ProductionDate = DateTime.Now;
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Production Time {ProductionDate} \nSerial Number: {SerialNumber} \nName: {Name} \nDescription: {Description} \nOperating System: {OperatingSystem}");
+        }
+        public abstract string GetDeviceName();
+    }
+
+    public class CellPhone : BaseDevice
+    {
+        public bool IsPhoneTRLicance { get; set; }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Tr Licance: " + IsPhoneTRLicance);
+        }
+
+        public override string GetDeviceName()
+        {
+            return "Cellphone Name --->" + Name;
+            
+        }
+
+    }
+
+    public class Computer : BaseDevice
+    {
+        private int usbPortCount;
+        public int UsbPortCount
+        {
+            get { return usbPortCount; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    usbPortCount = value;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Warning!: Invalid USB port count. Defaulting to -1.");
+                    Console.ResetColor();
+                    usbPortCount = -1;
+                }
+            }
+        }
+        public bool IsComputerBluetooth { get; set; }
+
+        public override string GetDeviceName()
+        {
+            return "Computer Name ---> " + Name;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Usb Port Count: " + usbPortCount +"\nBluetooth: " + IsComputerBluetooth);
+        }
+
+    }
+
+}
---
+++ Week4_Final/Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System.ComponentModel.Design;
+using Week4_Final;
+while (true)
+{
+que: Console.WriteLine("\nPlease select the device you want to produce. \n\n--Press 1 for CellPhone \n--Press 2 for Computer");
+    int input = Convert.ToInt32(Console.ReadLine());
+
+    if (input == 1)
+    {
+        CellPhone cellPhone = new CellPhone();
+
+        Console.WriteLine("-What is the serial number of the phone?");
+        cellPhone.SerialNumber = Console.ReadLine();
+
+        Console.WriteLine("-What is the name of the phone?");
+        cellPhone.Name = Console.ReadLine();
+        Console.WriteLine("-What is the description of the phone?");
+        cellPhone.Description = Console.ReadLine();
+        Console.WriteLine("-What is the operating system of the phone?");
+        cellPhone.OperatingSystem = Console.ReadLine();
+        Console.WriteLine("-Does the phone have a Tr licese? (y/n)");
+        string selection = Console.ReadLine();
+
+        if (selection == "y")
+            cellPhone.IsPhoneTRLicance = true;
+        else
+            cellPhone.IsPhoneTRLicance = false;
+
+        cellPhone.GetDeviceName();
+        cellPhone.ShowInfo();
+
+
+    }
+    else if (input == 2)
+    {
+        Computer computer = new Computer();
+
+        Console.WriteLine("-What is the serial number of the computer?");
+        computer.SerialNumber = Console.ReadLine();
+
+        Console.WriteLine("-What is the name of the computer?");
+        computer.Name = Console.ReadLine();
+        Console.WriteLine("-What is the description of the computer?");
+        computer.Description = Console.ReadLine();
+        Console.WriteLine("-What is the operating system of the computer?");
+        computer.OperatingSystem = Console.ReadLine();
+        Console.WriteLine("-How many usb ports the computer have?");
+        computer.UsbPortCount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("-Does the computer have bluetooth? (y/n)");
+        string answ = Console.ReadLine();
+
+        if (answ == "y")
+            computer.IsComputerBluetooth = true;
+        else
+            computer.IsComputerBluetooth = false;
+        computer.GetDeviceName();
+        computer.ShowInfo();
+
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("----------!!!Invalid input!!!----------");
+        Console.ResetColor();
+        goto que;
+    }
+    Console.WriteLine("\n\nDevice production successful. Would you like to produce another device ? (y/n)");
+    string main = Console.ReadLine();
+    if (main == "y")
+        goto que;
+    else { 
+        Console.WriteLine("\n\nHave a nice day!");
+    break;
+}
+        
+}
